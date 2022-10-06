36ccf69a2f18111a833f6e8fc8d504414994c512
abreham1222 <abreham1222@gmail.com>
2022-10-06 20:44:58 +0300

    fix linters

--- webpack-file/src/index.js
+++ webpack-file/src/index.js
@@ -1,47 +1,5 @@
 import './style.css';
-import Todo from './component';
-
-/*const list = [
-  {
-    description: 'Submit code',
-    index: 0,
-    complete: true,
-  },
-  {
-    description: 'Meet friends',
-    index: 1,
-    complete: true,
-  },
-  {
-    description: 'Go to gym',
-    index: 2,
-    complete: false,
-  },
-  {
-    description: 'Sleep',
-    index: 3,
-    complete: false,
-  },
-];
-const ul = document.querySelector('#myUL');
-// Create element
-const generate = (descValue) => {
-  const li = document.createElement('li');
-  const check = document.createElement('input');
-  const label = document.createElement('label');
-  check.type = 'checkbox';
-  ul.appendChild(li);
-  li.innerHTML = descValue;
-  li.appendChild(label);
-  label.appendChild(check);
-  check.setAttribute('class', 'check');
-  label.setAttribute('class', 'label');
-  li.setAttribute('class', 'items');
-};
-list.forEach((p) => {
-  generate(p.description, p.complete, p.index);
-});
-*/
+import Todo from './component.js';
 
 const newtodo = new Todo();
 newtodo.value = '';
