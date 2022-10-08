68406d23f6a682dbbc14af02baf284ce67c6146d
abreham1222 <abreham1222@gmail.com>
2022-10-08 13:41:39 +0300

    Add new module

--- webpack-file/src/index.js
+++ webpack-file/src/index.js
@@ -3,8 +3,3 @@ import Todo from './component.js';
 
 const newtodo = new Todo();
 newtodo.value = '';
-const label = document.querySelectorAll('.task');
-label.forEach((input, index) => input.addEventListener('click', () => {
-  const removeBtn = document.querySelectorAll('.remove-btn');
-  removeBtn[index].style.display = 'inline';
-}));
